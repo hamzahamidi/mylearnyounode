51d34b3a7b14903c398594093aaa063a35973486
Hamza HAMIDI <hamidihamzacg@gmail.com>
2018-01-17 12:44:23 +0100

    Add comments about adding thourgh2-map
    Signed-off-by: Hamza HAMIDI <hamidihamzacg@gmail.com>

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -1,5 +1,16 @@
 const http = require('http');
 const fs   = require('fs');
+/**
+ * through2-map is an external library. To add it you either have to install it globally
+ * by running the command $ npm i through2-map -s
+ * 
+ * Or in my case I download the library from github repository https://github.com/brycebaril/through2-map
+ * I created a folder node_modules in the root of the project  
+ * I copied the library through2-map then I placed the command line in node_modules and then I run $ npm install
+ * 
+ * Both the methods work!
+ * 
+ */
 const map  = require('through2-map');
 
 const port = process.argv[2];
